0f906e2e329c993826d6e2e5014f113a0ecd33e0
VKenji-pa <84997899+VKenji-pa@users.noreply.github.com>
2023-06-03 16:05:31 -0300

    Exercício 2: Teste de Integração

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,22 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('testando cadastro de uma pergunta e uma resposta para ela', () => {
+  modelo.cadastrar_pergunta('Qual a molecula do octano?');
+  modelo.cadastrar_resposta(0, 'C8H18');
+  const perguntas = modelo.listar_perguntas();
+  const respostas = modelo.get_respostas(0)
+  expect(perguntas[0].texto).toBe('Qual a molecula do octano?');
+  expect(respostas.length).toBe(1);
+  expect(respostas[0].texto).toBe('C8H18');
+});
+
+test('testando cadastro de tres perguntas e a pesquisa pela 2a pergunta usando id', () => {
+  modelo.cadastrar_pergunta('De onde vem o Jhin, de League of Legends');
+  modelo.cadastrar_pergunta('Quantos balas tem na arma do Jhin?');
+  modelo.cadastrar_pergunta('10 x 10 = ?');
+  const perguntas = modelo.listar_perguntas();
+  const segundaP = modelo.get_pergunta(perguntas[1].id_pergunta);
+  expect(segundaP.texto).toBe('Quantos balas tem na arma do Jhin?') 
+});
